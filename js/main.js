274fe0a63fd7cdd58866864eb159ef66aea06844
Vl-Wouter <woutvlae@student.arteveldehs.be>
2019-09-12 23:11:24 +0200

    Added location and weather

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const storage = window.localStorage
 // Search bar animation
 const searchLogo = document.querySelector('#searchLogo')
 const search = document.querySelector('#searchContainer')
@@ -26,3 +27,80 @@ function searchIt(e) {
     
 }
 searchForm.addEventListener('submit', searchIt)
+
+// Clock
+const clockItem = document.querySelector('.clock')
+
+const getDate = () => {
+    const today = new Date()
+    let hour = today.getHours()
+    let minute = today.getMinutes()
+    if(hour < 10) hour = `0${hour}`
+    if(minute < 10) minute = `0${minute}`
+
+    clockItem.innerHTML = `${hour} : ${minute}`
+}
+
+getDate()
+setInterval(getDate, 30000)
+
+// Setting Modal
+const modal = document.querySelector('#settingModal')
+const settingsIcon = document.querySelector('#settingsIcon')
+
+const openModal = () => {
+    modal.classList.remove('--hidden')
+}
+
+const closeModal = () => {
+    modal.classList.add('--hidden')
+}
+
+modal.addEventListener('click', function(e) {
+    if(e.target == modal) {
+        closeModal()
+    }
+})
+settingsIcon.addEventListener('click', openModal)
+
+// location and weather
+const locationItem = document.querySelector('.location')
+const weatherItem = document.querySelector('.weather')
+const tempItem = document.querySelector('.temp')
+const keybutton = document.querySelector('#addApiKeyBtn')
+
+const storeKey = () => {
+    const key = document.querySelector('#apiKey').value
+    storage.setItem('weatherApi', key)
+    closeModal()
+    getLocation()
+}
+
+const getLocation = () => {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(getWeather)
+    } else {
+
+    }
+}
+
+const getWeather = (position) => {
+    const {latitude, longitude} = position.coords
+    const apiKey = storage.getItem('weatherApi')
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`)
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        console.log(data)
+        locationItem.innerHTML = `${data.name}, ${data.sys.country}`
+        const weather = data.weather[0]
+        weatherItem.innerHTML = `<img src='http://openweathermap.org/img/wn/${weather.icon}@2x.png' alt='${weather.description}' />`
+        tempItem.innerHTML = `${parseInt(data.main.temp)}°C`
+    })
+
+}
+
+storage.getItem('weatherApi') ? getLocation() : openModal()
+keybutton.addEventListener('click', storeKey)
